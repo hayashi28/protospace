8069925318f6fca2279283b4b1ac12ea465ca9d1
林侑希 <hayashi@hayashiyuuki-no-MacBook-Pro.local>
2016-08-04 01:05:51 +0900

    make user_page_funciton

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root 'prototypes#index'
+  resources :users, only: :show
 
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
   get  '/prototypes/show'   =>    'prototypes#show'
+
+  root 'prototypes#index'
+
 end
