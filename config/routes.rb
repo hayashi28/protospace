13b8a86fa53afef62e1785329e0820b5756e66e2
林侑希 <hayashi@hayashiyuuki-no-MacBook-Pro.local>
2016-08-05 00:27:45 +0900

    make user_edit_function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'prototypes#index'
 
   devise_for :users
-  resources :users, only: :show
+  resources :users, only: [:show, :edit]
 
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
