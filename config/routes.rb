b50765a1315f7e1311e4e266fcdf395dd498c2dd
林侑希 <hayashi@hayashiyuuki-no-MacBook-Pro.local>
2016-08-04 23:14:51 +0900

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'prototypes#index'
+
   devise_for :users
   resources :users, only: :show
 
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
   get  '/prototypes/show'   =>    'prototypes#show'
-
-  root 'prototypes#index'
-
 end
