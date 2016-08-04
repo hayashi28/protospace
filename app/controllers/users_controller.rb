cab446a8e24e180217c745e1a7255bee8e070baa
林侑希 <hayashi@hayashiyuuki-no-MacBook-Pro.local>
2016-08-05 01:26:59 +0900

    change routes and user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  before_action :user, only: [:show, :edit]
 
   def show
     @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def user
     @user = User.find(params[:id])
   end
 
