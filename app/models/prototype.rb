c35a885e90c6d0d8e51616d797072c058d51b31d
林侑希 <hayashi@hayashiyuuki-no-MacBook-Pro.local>
2016-08-23 18:03:17 +0900

    delete file and add validation

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,5 @@
 class Prototype < ActiveRecord::Base
+  validates :title, :catchcopy, :concept, presence: true
   belongs_to :user
   has_many :prototype_images
   accepts_nested_attributes_for :prototype_images
--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -1,4 +1,5 @@
 class PrototypeImage < ActiveRecord::Base
+  validates :image, presence: true
   mount_uploader :image, ImageUploader
   belongs_to :prototype
   enum role: %i(main sub)
