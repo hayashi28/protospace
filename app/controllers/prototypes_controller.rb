7a6be52f3b252367c7e99cdba1188b499459a2e3
林侑希 <hayashi@hayashiyuuki-no-MacBook-Pro.local>
2016-08-29 17:58:43 +0900

    change prototypes_controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
   def index
-    @prototypes = Prototype.order("created_at DESC")
+    @prototypes = Prototype.order(created_at: :DESC)
   end
 
   def show
