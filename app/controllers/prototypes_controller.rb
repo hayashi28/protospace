713390a85822789cd505873c654db8c9a6eadc07
林侑希 <hayashi@hayashiyuuki-no-MacBook-Pro.local>
2016-09-02 11:05:34 +0900

    make prototype_edit_function

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,11 @@
 class PrototypesController < ApplicationController
+  before_action :define_prototype, except: [:index, :new, :create]
 
   def index
     @prototypes = Prototype.order(created_at: :DESC)
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
   end
 
   def new
@@ -23,10 +23,22 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to root_path, notice: 'Updated successfully'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catchcopy, :concept, :user_id, prototype_images_attributes: [:id, :image, :role, :prototype_id])
+  end
+
+  def define_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
 end
