035d38376fdd9a37ff600bff4999d6c96633611f
hayashi28 <ellegarden1004@gmail.com>
2016-10-10 01:32:48 +0900

    edit application_helper.rb

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,7 +5,7 @@ class Prototype < ActiveRecord::Base
   has_many :prototype_images, dependent: :delete_all
   has_many :likes, dependent: :destroy
   has_many :comments
-  accepts_nested_attributes_for :prototype_images, reject_if: :reject_sub_images, limit: 4
+  accepts_nested_attributes_for :prototype_images, reject_if: :reject_sub_images
 
   acts_as_taggable
 
